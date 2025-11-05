4d3e846f409cc46436439310afc6aa7511b06d8c
gal arbel <84223483+galarb@users.noreply.github.com>
2025-11-05 07:12:33 +0200

    Update gcode-parser.ino

--- gcode-parser/gcode-parser.ino
+++ gcode-parser/gcode-parser.ino
@@ -2,23 +2,16 @@
 
 gcodeparser parser(Serial);
 
+#define DIRY 5
+#define STEPY 2
+#define DIRX 6
+#define STEPX 3
+#define LIMITX 9
+#define LIMITY 10
+
 void setup() {
     Serial.begin(115200);
-    parser.setup(11, 5, 2, 6, 3, 20);
-    Serial.println("PARSING! STARTED INIT");
-
-    pinMode(2, OUTPUT);
-    pinMode(5, OUTPUT);
-
-    /*digitalWrite(5, HIGH);
-
-    for (int i = 0; i < 1000; i++){
-        digitalWrite(2, HIGH);
-        delayMicroseconds(500);
-        digitalWrite(2, LOW);
-        delayMicroseconds(500);
-        Serial.println("Pulse Sent!");
-    }*/
+    parser.setup(11, DIRX, STEPX, DIRY, STEPY, LIMITX, LIMITY, 20);
 }
 
 void loop() {
